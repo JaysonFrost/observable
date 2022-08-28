2f4bd675722551c41a415e60fe21aae34732a1a4
jaysonfrost <jaysonfrost@yandex.ru>
2022-08-28 14:44:01 +0000

    Update 111 files

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,21 @@ import track from ".";
 
 describe("Test track", () => {
   describe("track params", () => {
-    it("track doesnt call with wrond eventType", () => {
+    it("track doesnt call with wrong eventType", () => {
       const observer = track({
         targetSelector: "button",
-        eventType: "gdf"
+        eventType: "gdf",
       });
 
       expect(observer).toBe(undefined);
+    });
+
+    it("default eventType 'click' if it not passed", () => {
+      const observer = track({
+        targetSelector: "button",
+      });
+
+      expect(observer).toBeTruthy();
     });
   });
 });
